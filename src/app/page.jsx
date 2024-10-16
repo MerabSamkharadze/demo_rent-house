6fc86d687aff46094138d877ac55a95a7d86ceb4
MerabSamkharadze <samkharadzemerab@gmail.com>
2024-10-16 22:13:50 +0400

    make card dinamic

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,18 @@
 import Card from "@/components/Card";
+import image from "@/public/images/h1.png";
 
 export default function Home() {
-  return <Card />;
+  return (
+    <div className=" w-full h-screen flex items-center justify-center">
+      <Card
+        imgSrc={image}
+        price={"80 000"}
+        addres={"თბილისი, ი. ჭავჭავაძის 53"}
+        rooms={"2"}
+        vector={"0160"}
+        space={"55"}
+        forRent
+      />
+    </div>
+  );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,44 @@
 import Image from "next/image";
-import image from "@/public/images/house.jpg";
+import image from "@/public/images/h1.png";
 import Lokation from "@/public/svg/Lokation";
 import Bed from "@/public/svg/Bed";
 import Vector from "@/public/svg/Vector";
 import Vector2 from "@/public/svg/Vector2";
 
-export default function Card() {
+export default function Card({
+  imgSrc,
+  price,
+  addres,
+  rooms,
+  vector,
+  space,
+  forRent,
+}) {
   return (
-    <div className="w-96 h-[455px] flex-col justify-start items-start inline-flex">
+    <div className="w-96 h- h-[455px] flex-col justify-start items-start inline-flex ">
+      <div className="absolute mt-5 ml-5 w-[90px] h-[26px] p-1.5 bg-[#021526]/50 rounded-[15px] justify-center items-center gap-2.5 inline-flex">
+        <div className="text-center text-white text-xs font-medium font-['FiraGO'] tracking-wide">
+          {forRent ? "ქირავდება" : "iyideba"}
+        </div>
+      </div>
       <Image
         alt="home-image"
         width={1000}
         height={1000}
-        className="self-stretch h-[307px] rounded-tl-[14px] rounded-tr-[14px]"
-        src={image}
+        className=" self-stretch h-[307px] rounded-tl-[14px] rounded-tr-[14px]"
+        src={imgSrc}
       />
 
       <div className="self-stretch h-[148px] px-[25px] py-[22px] bg-white rounded-bl-[14px] rounded-br-[14px] border-l border-r border-b border-[#dadada] flex-col justify-center items-start gap-5 flex">
         <div className="self-stretch h-[60px] flex-col justify-start items-start gap-1.5 flex">
           <div className="self-stretch text-[#021526] text-[28px] font-bold font-['FiraGO']">
-            80 000 ₾
+            {price} ₾
           </div>
           <div className="self-stretch justify-start items-center gap-1 inline-flex">
             <Lokation />
 
             <div className="text-[#021526]/70 text-base font-normal font-['FiraGO']">
-              თბილისი, ი. ჭავჭავაძის 53
+              {addres}
             </div>
           </div>
         </div>
@@ -33,14 +46,14 @@ export default function Card() {
           <div className="h-6 justify-start items-center gap-[5px] flex">
             <Bed />
             <div className="text-[#021526]/70 text-base font-normal font-['FiraGO']">
-              2
+              {rooms}
             </div>
           </div>
           <div className="justify-start items-center gap-[5px] flex">
             <div className="justify-start items-center gap-1 flex">
               <Vector />
               <div className="text-[#021526]/70 text-base font-normal font-['FiraGO']">
-                55 მ
+                {space} მ
               </div>
 
               <div className="text-[#021526]/70 text-[10px] font-normal font-['FiraGO']">
@@ -51,7 +64,7 @@ export default function Card() {
           <div className="justify-start items-center gap-[5px] flex">
             <Vector2 />
             <div className="text-[#021526]/70 text-base font-normal font-['FiraGO']">
-              0160
+              {vector}
             </div>
           </div>
         </div>
