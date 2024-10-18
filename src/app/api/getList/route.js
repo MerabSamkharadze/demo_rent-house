1c454aa996c419252569ba5918d54023784be7e0
Daviti0410 <lazariashvilidato@gmail.com>
2024-10-18 12:55:59 +0400

    დავამატე გაფილტვრის ფუნქცია GET request-ს

--- src/app/api/getList/route.js
+++ src/app/api/getList/route.js
@@ -1,15 +1,23 @@
 import { NextResponse } from "next/server";
 import pool from "@/libs/connection";
 
-export async function GET() {
+export async function GET(req) {
+  const { searchParams } = new URL(req.url);
+  const id = searchParams.get("id");
   try {
     let sql = `SELECT * FROM rooms`;
+    const values = [];
 
-    const [results] = await pool.query(sql);
+    if (id) {
+      sql += ` WHERE id = ?`;
+      values.push(id);
+    }
+
+    const [results] = await pool.query(sql, values);
 
     const products = results.map((result) => ({
       ...result,
-      image: result.image.toString("base64"),
+      image: result.image ? result.image.toString("base64") : null,
     }));
 
     if (products.length === 0) {
